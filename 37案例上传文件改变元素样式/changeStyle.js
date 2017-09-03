0d044732f141f10e252262e32f29e1695b0bedb3
thewindwing <1049084067@qq.com>
2017-09-04 00:54:20 +0800

    bz

---
+++ 37案例上传文件改变元素样式/changeStyle.js
@@ -0,0 +1,8 @@
+
+//点击按钮设置div的样式
+document.getElementById("btn").onclick=function () {
+    var dvObj=document.getElementById("dv");
+    dvObj.style.width="400px";
+    dvObj.style.height="400px";
+    dvObj.style.backgroundColor="green";
+};
